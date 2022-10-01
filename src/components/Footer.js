089153b6793536f16173a66b031f3ea5091567ad
Brooklyne Cheney <brookecheney@hotmail.com>
2022-10-01 08:58:35 -0600

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,34 +1,15 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { Grid, Box, Text, Image, VStack } from '@chakra-ui/react'
-
+import { Progress } from "@chakra-ui/react";
+import "./Footer.css";
 function Footer() {
   return (
     <Box backgroundColor="#f3f1ef">
-        <Grid templateColumns={["repear(1, 1fr)","repeat(3, 1fr)"]} color="white" fontWeight="bold">
-
-        <VStack p="2rem">
-            <Link to="/">About</Link>
-            <Link to="/">Careers</Link>
-            <Link to="/">Contact</Link>
-        </VStack>
-        <VStack p="2rem">
-            <Link to="/">Twitter</Link>
-            <Link to="/">Instagram</Link>
-            <Link to="/">Facebook</Link>
-        </VStack>
-        <VStack p="2rem">
-            <Link to="/">Twitter</Link>
-            <Link to="/">Instagram</Link>
-            <Link to="/">Facebook</Link>
-        </VStack>
-        </Grid>
-        <Box>
-            <Text textAlign="center" color="white" w="100%" borderTop="1px solid white" p="1rem">
-                Copyright www.workingwithshopify.com
-            </Text>
-        </Box>
+      <footer class="footer"></footer>
     </Box>
+
+  
   )
 }
 
