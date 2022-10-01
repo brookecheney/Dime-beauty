2ea9676fc4cd6fb460e24aa1763a0a73d2418a98
Brooklyne Cheney <brookecheney@hotmail.com>
2022-10-01 09:14:04 -0600

    formatting

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,20 +5,19 @@ import { Progress } from "@chakra-ui/react";
 import "./Footer.css"
 import myImage from './progressbar.png'
 function Footer() {
-  return (
-    <Box backgroundColor="#f3f1ef">
-         <div class="wrapper">
-            <h1 class="h1-1">Add $100 to save 10%</h1>
-          <div class="push">
+    return (
+        <Box backgroundColor="#f3f1ef">
+            <div class="wrapper">
+                <h1 class="h1-1">Add $100 to save 10%</h1>
+                <div class="push">
+                    <Image src={myImage} class="image" />
+                </div>
+            </div>
+            <footer class="footer"></footer>
+        </Box>
 
-            <Image src={myImage} class="image" />
-          </div>
-          </div>
-      <footer class="footer"></footer>
-    </Box>
 
-  
-  )
+    )
 }
 
 export default Footer
