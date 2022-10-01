e197d7975e0c66d30da2903dedddf475e4ffd20f
Brooklyne Cheney <brookecheney@hotmail.com>
2022-10-01 10:06:06 -0600

    spacing

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -28,71 +28,73 @@ const Home = () => {
   if (!products) return <div>Loading...</div>;
   return (
     <body>
-  <main>
-    <Box>
-      <Grid p="1rem" templateColumns={["repeat(2, 2fr)", "repeat(2, 2fr)"]}>
-        {products.map((product) => (
-          <Link p="2rem" to={`/products/${product.handle}`} key={product.id} margin-left="auto">
-            <Box
-              maxW="sm" borderWidth="1px" rounded="lg" overflow="hidden"
-              _hover={{ opacity: "80%" }}
-              textAlign="left"
-              position="relative"
-              width="240px"
-              height="400px"
-              borderRadius="20px"
-            >
-              <Image src={product.images[0].src} />
-              <Text textAlign="left" position="absolute" p="1rem" bottom="24%" w="100%" fontWeight="bold">
-                {product.title}
-              </Text>
-              <Box d="flex" p="2rem" mt="2" alignItems="center" bottom="-6%">
-                {Array(5)
-                  .fill("")
-                  .map((_, i) => (
-                    <StarIcon
-                      key={i}
-                      color={i < 3 ? "#2d2d2d" : "#2d2d2d"}
-                    />
-                  ))}
-                <Box as="span" h={8} ml="2" color="#2d2d2d" fontSize="sm">
-                  7 reviews
+      <main>
+        <Box>
+          <Grid templateColumns={["repeat(2, 2fr)", "repeat(2, 2fr)"]}>
+            {products.map((product) => (
+              <Link m={2} p="2rem" to={`/products/${product.handle}`} key={product.id} margin-left="auto">
+                <Box
+                  m={2}
+                  boxShadow="lg"
+                  maxW="sm" borderWidth="1px" rounded="lg" overflow="hidden"
+                  _hover={{ opacity: "80%" }}
+                  textAlign="left"
+                  position="relative"
+                  width="240px"
+                  height="400px"
+                  borderRadius="20px"
+                >
+                  <Image src={product.images[0].src} />
+                  <Text textAlign="left" position="absolute" p="1rem" bottom="24%" w="100%" fontWeight="bold">
+                    {product.title}
+                  </Text>
+                  <Box d="flex" p="2rem" mt="2" alignItems="center" bottom="-6%">
+                    {Array(5)
+                      .fill("")
+                      .map((_, i) => (
+                        <StarIcon
+                          key={i}
+                          color={i < 3 ? "#2d2d2d" : "#2d2d2d"}
+                        />
+                      ))}
+                    <Box as="span" h={8} ml="2" color="#2d2d2d" fontSize="sm">
+                      7 reviews
+                    </Box>
+                    <Text fontWeight="bold">${product.variants[0].price}</Text>
+                  </Box>
+                  <Button
+                    onClick={() => addItemToCheckout(product.variants[0].id, 1)}
+                    _hover={{ opacity: "70%" }}
+                    w="14rem"
+                    p="1rem"
+                    bottom="3%"
+                    backgroundColor="#2d2d2d"
+                    color="white"
+                  >
+                    Add To Bundle
+                  </Button>
                 </Box>
-                <Text fontWeight="bold">${product.variants[0].price}</Text>
-              </Box>
-              <Button
-                onClick={() => addItemToCheckout(product.variants[0].id, 1)}
-                _hover={{ opacity: "70%" }}
-                w="14rem"
-                p="1rem"
-                bottom="3%"
-                backgroundColor="#2d2d2d"
-                color="white"
-              >
-                Add To Bundle
-              </Button>
-            </Box>
 
-          </Link>
-          
-        ))}
-       
-      </Grid>
-      </Box>
-   
-    </main>
+              </Link>
 
-     <footer class="footer">
+            ))}
 
-     <h1 class="h1-1">Add $100 to save 10%</h1>
-     
-     <Image src={myImage} class="image" />
-     
-     
-     
-     </footer>
+          </Grid>
+        </Box>
 
-     </body>
+      </main>
+
+      <footer class="footer">
+
+        <h1 class="h1-1">Add $100 to save 10%</h1>
+
+        <Image src={myImage} class="image" />
+
+
+
+      </footer>
+
+    </body>
   );
 };
 
