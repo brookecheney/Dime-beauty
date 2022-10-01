c08559ca10bb38d44fbb65be82f9e4387ff5cb27
Brooklyne Cheney <brookecheney@hotmail.com>
2022-10-01 09:08:38 -0600

    add progress bar

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,13 +2,17 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { Grid, Box, Text, Image, VStack } from '@chakra-ui/react'
 import { Progress } from "@chakra-ui/react";
-import "./Footer.css";
+import "./Footer.css"
+import myImage from './progressbar.png'
 function Footer() {
   return (
     <Box backgroundColor="#f3f1ef">
          <div class="wrapper">
-            <h1>Add $100 to save 10%</h1>
-          <div class="push"></div>
+            <h1 class="h1-1">Add $100 to save 10%</h1>
+          <div class="push">
+
+            <Image src={myImage} />
+          </div>
           </div>
       <footer class="footer"></footer>
     </Box>
