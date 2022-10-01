e258c2d0a4eb59534403b7c13ea812ba68c6b69f
Brooklyne Cheney <brookecheney@hotmail.com>
2022-10-01 09:01:12 -0600

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,6 +6,10 @@ import "./Footer.css";
 function Footer() {
   return (
     <Box backgroundColor="#f3f1ef">
+         <div class="wrapper">
+            <h1>Add $100 to save 10%</h1>
+          <div class="push"></div>
+          </div>
       <footer class="footer"></footer>
     </Box>
 
